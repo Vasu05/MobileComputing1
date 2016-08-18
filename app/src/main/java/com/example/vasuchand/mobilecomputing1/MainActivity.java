45d8798c00b075613ad68ae61696dd1793b14d5a
Vasu <Vasu14114@iiitd.ac.in>
2016-08-18 19:59:17 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/vasuchand/mobilecomputing1/MainActivity.java
+++ app/src/main/java/com/example/vasuchand/mobilecomputing1/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
